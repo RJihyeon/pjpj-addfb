5d67319af8cf54b6b15770f93151164452118971
ellie3413 <145912699+ellie3413@users.noreply.github.com>
2023-11-13 16:53:49 +0900

    1113

---
+++ src/components/HeadComponent/HeaderDropdown.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import useDetectClose from "./useDetectClose";
+
+const HeaderDropdown = () => {
+  const [myPageIsOpen, myPageRef, myPageHandler] = useDetectClose(false);
+  const [boardIsOpen, boardRef, boardHandler] = useDetectClose(false);
+
+  const logoutClickHandler = () => {
+    console.log("logout");
+  };
+
+  return (
+    <Wrapper>
+      <DropdownContainer>
+        <DropdownButton onClick={myPageHandler} ref={myPageRef}>
+          마이페이지
+        </DropdownButton>
+        <Menu isDropped={myPageIsOpen}>
+          <Ul>
+            <Li>
+              <LinkWrapper href="/mypage">회원정보수정</LinkWrapper>
+            </Li>
+            <Li>
+              <LinkWrapper href="/myreservpage">예약관리</LinkWrapper>
+            </Li>
+            
+          </Ul>
+        </Menu>
+      </DropdownContainer>
+
+    </Wrapper>
+  );
+};
+
+export default HeaderDropdown;
+
+const Wrapper = styled.div`
+  margin-right: 0px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+  font-size: 14px;
+  background:  #1c1c1c;
+  width: 70px;
+  height: 50px;
+  
+`;
+
+const DropdownContainer = styled.div`
+  position: relative;
+  text-align: center;
+`;
+
+const DropdownButton = styled.div`
+  cursor: pointer;
+`;
+
+const Menu = styled.div`
+  background: gray;
+  position: absolute;
+  top: 52px;
+  left: 50%;
+  width: 100px;
+  text-align: center;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 3px;
+  opacity: 0;
+  visibility: hidden;
+  transform: translate(-50%, -20px);
+  transition: opacity 0.4s ease, transform 0.4s ease, visibility 0.4s;
+  z-index: 9;
+
+  &:after {
+    content: "";
+    height: 0;
+    width: 0;
+    position: absolute;
+    top: -3px;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border: 12px solid transparent;
+    border-top-width: 0;
+    border-bottom-color: gray;
+  }
+
+  ${({ isDropped }) =>
+    isDropped &&
+    css`
+      opacity: 1;
+      visibility: visible;
+      transform: translate(-50%, 0);
+      left: 50%;
+    `};
+`;
+
+const Ul = styled.ul`
+  & > li {
+    margin-bottom: 10px;
+  }
+
+  & > li:first-of-type {
+    margin-top: 10px;
+  }
+
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Li = styled.li``;
+
+const LinkWrapper = styled.a`
+  font-size: 14px;
+  text-decoration: none;
+  color: white;
+`;
+
+
--- src/pages/MyReservPage.jsx
+++ src/pages/MyReservPage.jsx
@@ -6,7 +6,7 @@ import NavComponent from "../components/NavComponent/NavComponent";
 function MyReservPage() {
   return (
     <div>
-      <NavComponent headerText="My 예약" bannerText="My 예약" />
+      <NavComponent headerText="예약관리" bannerText="예약관리" />
       <p>예약신청된 정보들을 여기에 불러옵니다</p>
       <button className="button">예약취소</button>
     
